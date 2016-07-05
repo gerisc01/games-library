7dc3911d58ae92c49728acb17884b81ad1f4350c
Scott Gerike <gerisc01@luther.edu>
2016-07-05 17:02:21 -0500

    Started the process of adding items from items.json to queue.html

--- items.js
+++ items.js
@@ -1,3 +1,29 @@
-function getItems() {
-    // SELECT name,length FROM items WHERE list="backlog"
+// Populates the lists with a class of "list" and the id of the lists passed as a parameter
+function populateLists(lists) {
+    $.getJSON("items.json", function(data) {
+        var listItems;
+        for (var i=0;i<lists.length;i++) {
+            listItems[lists[i]] = [];
+        }
+
+        var items = data["items"];
+        var itemsLen = items.length;
+        for (var j=0;j<itemsLen;j++) {
+            var item = items[j];
+            if (lists.indexOf(item["list"]) > -1) {
+                listItems[item["list"]] = item;
+            }
+        }
+
+        // Add the items to the list
+        for (var i=0;i<lists.length;i++) {
+            var listId = list[i];
+
+            // sort and add lists to queue.html
+            // items.sort(function(a,b) {
+            //     return a - b;
+            // });
+
+        }
+    });
 }
