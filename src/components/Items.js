40f93ac4f5e4d794aee33b659aefcd6576662f42
Scott Gerike <gerisc01@luther.edu>
2017-07-11 14:21:42 -0500

    Cancel button now fully works

--- src/components/Items.js
+++ src/components/Items.js
@@ -12,11 +12,15 @@ function Items({ items,activeList,editingId,onClicks }) {
         <ItemHeader fields={activeList.fields}/>
         {items.map(item => {
           if (item.id === editingId) {
+            // Make a copy of the item before passing it to ItemEdit so that editing the object
+            // doesn't edit the object in the state (making a copy of the whole items list makes
+            // a copy of the list, but still references the original state items)
+            const copyItem = Object.assign({},item)
             return <ItemEdit
               key={item.id}
               fields={activeList.fields}
-              item={item} 
-              acceptClick={() => onClicks.acceptEditingItem(item.id,item)}
+              item={copyItem}
+              acceptClick={() => onClicks.acceptEditingItem(item.id,copyItem)}
               cancelClick={() => onClicks.cancelEditingItem(item.id)} />
           } else {
             return <Item
