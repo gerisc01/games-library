e73c0404fd1ee49cc30054856b9d476a4c445311
Scott Gerike <gerisc01@luther.edu>
2017-08-11 17:14:42 -0500

    Moved Item edit from redux to local component state

--- src/components/ListComponents/ItemList.js
+++ src/components/ListComponents/ItemList.js
@@ -4,24 +4,37 @@ import ItemRow from './ItemRow'
 class ItemList extends React.Component {  
   constructor(props, context) {
     super(props, context);
-    this.itemEditingOnClicks = {
-      setEditingItem: props.onSetEditingItem,
-      acceptEditingItem: props.onAcceptEditingItem,
-      cancelEditingItem: props.onCancelEditingItem,
-    }
-    this.order = this.props.items.map(item => {return item.id})
+
+    // Populate items
     this.items = {}
-    this.props.items.forEach(item => {
-      this.items[item.id] = item
-    })
+    this.props.items.forEach(item => { this.items[item.id] = item })
+    this.state = {
+      editingId: undefined,
+      order: this.props.items.map(item => {return item.id}),
+      items: this.items,
+    };
+  }
+
+  startEditItem = (id) => {
+    this.setState({editingId: id})
+  }
+
+  acceptEditItem = (item) => {
+    let items = Object.assign({},this.state.items)
+    items[item.id] = item
+    this.setState({items: items, editingId: undefined})
+  }
+
+  cancelEditItem = () => {
+    this.setState({editingId: undefined})
   }
 
   render() {
   return (
     <div>
-      {this.order.map(id => {
-        return <ItemRow key={id} item={this.items[id]} fields={this.props.fields} 
-            isEditing={this.props.editingId === id} onClicks={this.itemEditingOnClicks} />
+      {this.state.order.map(id => {
+        return <ItemRow key={id} item={this.state.items[id]} fields={this.props.fields} isEditing={this.state.editingId == id}
+          startEditItem={this.startEditItem} acceptEditItem={this.acceptEditItem} cancelEditItem={this.cancelEditItem} />
       })}
     </div>)
   }
--- src/components/ListComponents/ItemRow.js
+++ src/components/ListComponents/ItemRow.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ItemEdit from './ItemEdit'
 import Item from './Item'
 
-function ItemRow({ item,fields,isEditing,onClicks }) {
+function ItemRow({ item,fields,isEditing,startEditItem,acceptEditItem,cancelEditItem }) {
   return isEditing
     ? <ItemEdit
         fields={fields}
         item={item}
-        acceptClick={() => onClicks.acceptEditingItem(item.id,item)}
-        cancelClick={() => onClicks.cancelEditingItem(item.id)} />
+        acceptClick={() => acceptEditItem(item)}
+        cancelClick={() => cancelEditItem()} />
     : <Item
         fields={fields}
         item={item}
-        editClick={() => onClicks.setEditingItem(item.id)} />
+        editClick={() => startEditItem(item.id)} />
 }
 export default ItemRow
